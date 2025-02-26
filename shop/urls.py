6e01981d56162889143269ddc8655e7206382b77
reallilboy <reallilboy696@gmail.com>
2025-02-26 19:51:09 +0330

    create login and logout user

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,9 @@ from django.urls import path,include
 from . import views as shop_views
 
 urlpatterns = [
-    path('',shop_views.home , name='home')
+    path('',shop_views.home , name='home'),
+    path('cart/',shop_views.cart , name='cart'),
+    path('login/',shop_views.login_user ,name='login'),
+    path('signup/',shop_views.signup_user ,name='signup'),
+    path('logout/',shop_views.logout_user ,name='logout')
 ]
